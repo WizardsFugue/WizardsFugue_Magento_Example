8f26d9e9c6f0877e34f12db9e39d8bc203431ede
Kristof Ringleff <kristof@fooman.co.nz>
2014-02-01 09:30:16 +1300

    switch to old school array notation

--- bootstrap.php
+++ bootstrap.php
@@ -32,21 +32,21 @@ unset( $originalIncludePath );
 $options = array();
 
 $options['config_model'] = '\WizardsFugue\Magenta1\Core\Model\Config';
-$options['wizards_fudge'] = [
-    'paths' => [
-        'wf_code_dir'   => __DIR__.DS.'mage'.DS.'app'.DS.'code',
+$options['wizards_fudge'] = array(
+    'paths' => array(
+        'wf_code_dir' => __DIR__ . DS . 'mage' . DS . 'app' . DS . 'code',
         //'wf_design_dir' => __DIR__.DS.'mage_htdocs'.DS.'app'.DS.'design',
         #'design_dir'    => __DIR__.DS.'mage'.DS.'app'.DS.'design',
-        'design_dir'    => __DIR__.DS.'/vendor/connect20/Interface_Frontend_Base_Default/app/design',
-        'skin_dir'      => __DIR__.DS.'public'.DS.'skin',
-        'etc_dir'       => __DIR__.DS.'mage'.DS.'app'.DS.'etc',
-        'var_dir'       => __DIR__.DS.'mage'.DS.'var',
-        'tmp_dir'       => __DIR__.DS.'mage'.DS.'var'.DS.'tmp',
-        'cache_dir'     => __DIR__.DS.'mage'.DS.'var'.DS.'cache',
-        'log_dir'       => __DIR__.DS.'mage'.DS.'var'.DS.'log',
-        'session_dir'   => __DIR__.DS.'mage'.DS.'var'.DS.'session',
+        'design_dir'  => __DIR__ . DS . '/vendor/connect20/Interface_Frontend_Base_Default/app/design',
+        'skin_dir'    => __DIR__ . DS . 'public' . DS . 'skin',
+        'etc_dir'     => __DIR__ . DS . 'mage' . DS . 'app' . DS . 'etc',
+        'var_dir'     => __DIR__ . DS . 'mage' . DS . 'var',
+        'tmp_dir'     => __DIR__ . DS . 'mage' . DS . 'var' . DS . 'tmp',
+        'cache_dir'   => __DIR__ . DS . 'mage' . DS . 'var' . DS . 'cache',
+        'log_dir'     => __DIR__ . DS . 'mage' . DS . 'var' . DS . 'log',
+        'session_dir' => __DIR__ . DS . 'mage' . DS . 'var' . DS . 'session',
 
-        'media_dir'     => __DIR__.DS.'public'.DS.'media',
-    ]
-];
+        'media_dir'   => __DIR__ . DS . 'public' . DS . 'media',
+    )
+);
 
